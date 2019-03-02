b9f9ef06265c872e058ea2c9318213f3431ef476
psychsane <psychsane@gmail.com>
2019-03-02 22:52:51 +0530

    Add @ToString sample

--- src/main/java/samples/ToStringDemo.java
+++ src/main/java/samples/ToStringDemo.java
@@ -1,4 +1,26 @@
 package samples;
 
+import lombok.ToString;
+
+@ToString
 public class ToStringDemo {
+
+    @ToString.Exclude
+    private int id;
+    private static final int STATIC_VAR = 10;
+    private String name;
+    private String[] tags = {"foo", "bar"};
+
+    public String getName() {
+        return "demo";
+    }
+
+    @ToString.Include
+    private long getRating() {
+        return Long.MIN_VALUE;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ToStringDemo());
+    }
 }
