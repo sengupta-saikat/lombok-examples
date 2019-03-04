ca1f7bf919f3c06ceb485267739b1fbb957a4eef
psychsane <psychsane@gmail.com>
2019-03-04 20:59:05 +0530

    Add @Data sample

---
+++ src/main/java/samples/DataDemo.java
@@ -0,0 +1,21 @@
+package samples;
+
+import lombok.Data;
+
+/**
+ * shortcut for @ToString, @EqualsAndHashCode, @Getter on all fields, @Setter on all non-final fields,
+ * and @RequiredArgsConstructor!
+ */
+
+@Data
+public class DataDemo {
+    private final String name;
+    private int age;
+    private String[] tags = {"dev", "ops"};
+
+    public static void main(String[] args) {
+        DataDemo demo = new DataDemo("QE");
+        demo.setAge(10);
+        System.out.println(demo.toString());
+    }
+}
